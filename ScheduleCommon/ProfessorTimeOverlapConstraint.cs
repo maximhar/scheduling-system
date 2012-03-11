199ed495caa4da1ab75b37fa58b63e36fb62ef92
Maxim Harizanov <maxim.harizanov@gmail.com>
2012-03-11 22:33:41 +0200

    Added two tests for ProfessorTimeOverlapConstraint and a dummy class to be implemented.

---
+++ ScheduleCommon/ProfessorTimeOverlapConstraint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScheduleCommon
+{
+    public class ProfessorTimeOverlapConstraint : IConstraint
+    {
+        public ProfessorTimeOverlapConstraint()
+        {
+        }
+        public ConstraintResult Check(Schedule aSchedule)
+        {
+            return new ConstraintResult(false, "Constraint not implemented yet.");
+        }
+    }
+}
