b3114c70dc4ddbd9bba871d9d1de5b55f0727dd2
mitelinko <mitelinko@gmail.com>
2012-03-14 22:50:42 +0200

    Implemented ProfessorDayConstraint (not tested yet).

--- ScheduleCommon/ProfessorDayConstraint.cs
+++ ScheduleCommon/ProfessorDayConstraint.cs
@@ -7,13 +7,36 @@ namespace ScheduleCommon
 {
     class ProfessorDayConstraint : IConstraint
     {
-        public ProfessorDayConstraint()
+        private Professor prof;
+        private List<int> off;
+        public ProfessorDayConstraint(Professor professor, List<int> daysOff)
         {
+            this.prof = professor;
+            this.off = daysOff;
         }
 
         public ConstraintResult Check(Schedule sched)
         {
-            return new ConstraintResult(false, "ProfessorDayConstraint not implemented yet!");
+            bool pass = true;
+            StringBuilder errorContainer = new StringBuilder();
+
+            foreach (int day in off)
+            {
+                foreach(var group in Configuration.Instance.Groups)
+                {
+                    for (int classs = 0; classs < sched[day][group].Count; classs++ )
+                    {
+                        if (sched[day][group][classs].Course.Professor == prof)
+                        {
+                            pass = false;
+                            string error = string.Format("Professor Days Off conflict: professor {0} conflicts in day {1}",
+                                sched[day][group][classs].Course.Professor, day);
+                            errorContainer.AppendLine(error);
+                        }
+                    }
+                }
+            }
+            return new ConstraintResult(pass, errorContainer.ToString());
         }
     }
 }
