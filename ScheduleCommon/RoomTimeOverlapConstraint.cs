81b6ee45fd733b1c2276a858f04a0372b6acba76
unknown <mitelinko@mitelinko-PC.(none)>
2012-03-14 15:55:49 +0200

    RoomTimeOverlapConstraint implemented (not tested).

--- ScheduleCommon/RoomTimeOverlapConstraint.cs
+++ ScheduleCommon/RoomTimeOverlapConstraint.cs
@@ -10,9 +10,42 @@ namespace ScheduleCommon
         public RoomTimeOverlapConstraint()
         {
         }
-        public ConstraintResult Check(Schedule aSchedule)
+        public ConstraintResult Check(Schedule sched)
         {
-            return new ConstraintResult(false, "Constraint not implemented yet.");
+            StringBuilder errorContainer = new StringBuilder();
+            for (int day = 0; day < 6; day++){ 
+                for (int groupN=0; groupN < Configuration.Instance.Groups.Count-1; groupN++){
+                    var group = Configuration.Instance.Groups[groupN];
+                    for (int classsN = 0; classsN < sched[day][group].Count; classsN++){
+                        int groupN2 = groupN;
+                        while (groupN2 < Configuration.Instance.Groups.Count)
+                        {
+                            var group2 = Configuration.Instance.Groups[groupN2];
+                            var classs = sched[day][group][classsN];
+                            var classs2 = sched[day][group2][classsN];
+
+                            if (classs.Room == classs2.Room)
+                            {
+
+                                var start1 = sched.GetStartTimeForClass(day, group, classs);
+                                var start2 = sched.GetStartTimeForClass(day, group2, classs2);
+                                if (start1 + classs.Length >= start2 && start2 <= start1 + classs.Length && start1+classs.Length<=start2 + classs2.Length)
+                                {
+                                    string error = string.Format("Room conflict: room {0} conflicts between group {1} and group {2} in day {3}",
+                                        classs.Room, group, group2, day);
+                                    errorContainer.AppendLine(error);
+                                }
+                            }
+                        }
+
+
+                    }
+                }
+            }
+
+
+
+                return new ConstraintResult(false, "Constraint not implemented yet.");
         }
     }
 }
