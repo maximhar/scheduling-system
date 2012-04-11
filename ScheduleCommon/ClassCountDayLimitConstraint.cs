8fa49e72058831a5d83416b21bcf27c13fb10b18
unknown <mitelinko@mitelinko-PC.(none)>
2012-04-11 18:07:56 +0300

    Implemented ClassCountDayLimitConstraint (not yet tested!)

--- ScheduleCommon/ClassCountDayLimitConstraint.cs
+++ ScheduleCommon/ClassCountDayLimitConstraint.cs
@@ -5,7 +5,46 @@ using System.Text;
 
 namespace ScheduleCommon
 {
-    class ClassCountDayLimitConstraint
+    [Serializable]
+    class ClassCountDayLimitConstraint : IConstraint
     {
+        private int classLimit;
+        public ClassCountDayLimitConstraint(int aClassLimit)
+        {
+            classLimit = aClassLimit;
+        }
+
+        public ConstraintResult Check(Schedule sched)
+        {
+            bool pass = true;
+            StringBuilder errorContainer = new StringBuilder();
+            for (int day = 0; day < 6; day++) 
+            {
+                if (sched[day].Count == 0)
+                {
+                    continue;
+                }
+
+                foreach (var group in Configuration.Instance.Groups)
+                {
+                    int ClassCount = sched[day][group].Count;
+                    if (ClassCount > classLimit)
+                    {
+                        pass = false;
+                        string error = string.Format("Class count exceeds limit of {0}: group {1} has {2} classes on day {3}",
+                            classLimit, group.Name, ClassCount, day);
+                        errorContainer.AppendLine(error);
+                    }
+                }
+
+
+
+            }
+
+
+
+                return new ConstraintResult(true, "Still testing");
+            //return new ConstraintResult(pass, errorContainer.ToString());
+        }
     }
 }
