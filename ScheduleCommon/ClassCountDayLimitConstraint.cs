1ad6f4f5affb626e5bc68a2a9c90a71de806586b
Maxim Harizanov <maxim.harizanov@gmail.com>
2012-04-21 20:05:25 +0300

    Merge branch 'master' of https://github.com/mitelinko/Maxim-s---Mitko-s-project-for-TP
    Conflicts:
    	ScheduleCommon/bin/Debug/ScheduleCommon.dll
    	ScheduleCommon/bin/Debug/ScheduleCommon.pdb
    	ScheduleCommon/obj/x86/Debug/ScheduleCommon.dll
    	ScheduleCommon/obj/x86/Debug/ScheduleCommon.pdb
    	ScheduleConcept.suo
    	ScheduleWPF/bin/Debug/ScheduleCommon.dll
    	ScheduleWPF/bin/Debug/ScheduleCommon.pdb
    	ScheduleWPF/bin/Debug/ScheduleWPF.exe
    	ScheduleWPF/bin/Debug/ScheduleWPF.pdb
    	ScheduleWPF/obj/x86/Debug/DesignTimeResolveAssemblyReferences.cache
    	ScheduleWPF/obj/x86/Debug/MainWindow.baml
    	ScheduleWPF/obj/x86/Debug/ScheduleWPF.exe
    	ScheduleWPF/obj/x86/Debug/ScheduleWPF.g.resources
    	ScheduleWPF/obj/x86/Debug/ScheduleWPF.pdb
    	Tests/bin/Debug/ScheduleCommon.dll
    	Tests/bin/Debug/ScheduleCommon.pdb
    	Tests/bin/Debug/Tests.exe
    	Tests/bin/Debug/Tests.pdb
    	Tests/obj/x86/Debug/Tests.exe
    	Tests/obj/x86/Debug/Tests.pdb

---
+++ ScheduleCommon/ClassCountDayLimitConstraint.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScheduleCommon
+{
+    [Serializable]
+    class ClassCountDayLimitConstraint : IConstraint
+    {
+        private int classLimit;
+        public ClassCountDayLimitConstraint(int aClassLimit)
+        {
+            classLimit = aClassLimit;
+        }
+
+        public ConstraintResult Check(Schedule sched)
+        {
+            bool pass = true;
+            StringBuilder errorContainer = new StringBuilder();
+            for (int day = 0; day < 6; day++) 
+            {
+                if (sched[day].Count == 0)
+                {
+                    continue;
+                }
+
+                foreach (var group in Configuration.Instance.Groups)
+                {
+                    int ClassCount = sched[day][group].Count;
+                    if (ClassCount > classLimit)
+                    {
+                        pass = false;
+                        string error = string.Format("Class count exceeds limit of {0}: group {1} has {2} classes on day {3}",
+                            classLimit, group.Name, ClassCount, day);
+                        errorContainer.AppendLine(error);
+                    }
+                }
+            }
+            return new ConstraintResult(pass, errorContainer.ToString());
+        }
+    }
+}
